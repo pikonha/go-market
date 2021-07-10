8d2138722289e7d4e96a65984505bff35dfa2a81
picolloo <lucaspicolloo@gmail.com>
2021-07-09 21:36:32 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# GO Market
+
+This is a market API implementation that follows DDD archicteture.
+
+The main goal here is to create an archicteture that allows to switch
+between different tools with no effort.
+
--- main.go
+++ main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-	"net/http"
+  "database/sql"
+  "fmt"
+  "log"
+  "net/http"
 
-	_ "github.com/lib/pq"
+  _ "github.com/lib/pq"
 
-	"github.com/gorilla/mux"
-	"github.com/picolloo/go-market/product/infra"
-	"github.com/picolloo/go-market/product/infra/ports"
-	"github.com/picolloo/go-market/product/usecases"
+  "github.com/gorilla/mux"
+  "github.com/picolloo/go-market/product/infra"
+  "github.com/picolloo/go-market/product/infra/ports"
+  "github.com/picolloo/go-market/product/usecases"
 )
 
 func main() {
-	dbUrl := fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		"go-market-db", 5432, "docker", "docker", "go-market",
-	)
+  dbUrl := fmt.Sprintf(
+    "host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+    "go-market-db", 5432, "docker", "docker", "go-market",
+  )
 
-	db, err := sql.Open("postgres", dbUrl)
-	if err != nil {
-		log.Fatalf("Unable to connect to database: %s", err.Error())
-	}
-	defer db.Close()
+  db, err := sql.Open("postgres", dbUrl)
+  if err != nil {
+    log.Fatalf("Unable to connect to database: %s", err.Error())
+  }
+  defer db.Close()
 
   postgresProductRepo := product_infra.NewPostgresProductRepository(db)
-	service := product_usecase.NewService(postgresProductRepo)
+  service := product_usecase.NewService(postgresProductRepo)
 
   rootRouter := mux.NewRouter()
 
@@ -40,17 +40,16 @@ func main() {
     ))
 
 
-  apiRouter := rootRouter.PathPrefix("/api").Subrouter()
-  product_ports.MakeProductHandlers(apiRouter, service)
+    apiRouter := rootRouter.PathPrefix("/api").Subrouter()
+    product_ports.MakeProductHandlers(apiRouter, service)
 
-  // rootRouter.Use(apiRouter)
-  http.Handle("/", rootRouter)
+    http.Handle("/", rootRouter)
 
-	server := &http.Server{
-		Addr: ":9000",
-	}
+    server := &http.Server{
+      Addr: ":9000",
+    }
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatal(err)
-	}
-}
+    if err := server.ListenAndServe(); err != nil {
+      log.Fatal(err)
+    }
+  }
